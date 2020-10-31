37e12f4837116a06c594517c6091402e0a704ffa
brianooi123 <ooikelvin3030@gmail.com>
2020-10-31 22:00:19 +0800

    31/10 ooi

--- app/src/main/java/com/example/neatshoe/Login.kt
+++ app/src/main/java/com/example/neatshoe/Login.kt
@@ -1,5 +1,7 @@
 package com.example.neatshoe
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
@@ -25,6 +28,7 @@ import java.util.regex.Pattern
 class Login : Fragment() {
     lateinit var editEmail: EditText
     lateinit var editPassword: EditText
+    lateinit var ForgetPassword: TextView
     lateinit var btnLogin: Button
     lateinit var btnRegLogin: Button
     private lateinit var mAuth: FirebaseAuth
@@ -42,49 +46,57 @@ class Login : Fragment() {
         editEmail = thisview.findViewById(R.id.editEmail)
         editPassword = thisview.findViewById(R.id.editPassword)
         btnLogin = thisview.findViewById(R.id.btLogin)
+        ForgetPassword =thisview.findViewById(R.id.forgetPassword)
         // Inflate the layout for this fragment
         btnRegLogin.setOnClickListener {view: View ->
 
             view.findNavController().navigate(R.id.action_login_to_register)
         }
+
+        ForgetPassword.setOnClickListener {view: View ->
+            view.findNavController().navigate(R.id.action_login_to_forgetpassword)
+
+        }
+        /*forgetPassword.setOnClickListener {
+            val builder =AlertDialog.Builder(requireActivity())
+            builder.setTitle("Forget Password")
+            val view =layoutInflater.inflate(R.layout.dialog_forgetpassword,null)
+            val userEmail=view.findViewById<EditText>(R.id.et_email)
+            builder.setView(view)
+            builder.setPositiveButton("Reset", DialogInterface.OnClickListener { _, _ ->
+                forgetPassword(userEmail)
+            })
+            builder.setNegativeButton("Close", DialogInterface.OnClickListener { _, _ ->  })
+        }*/
         btnLogin.setOnClickListener() {
 
             val useremail = editEmail.text.toString().trim()
             val password = editPassword.text.toString().trim()
             LoginWithAuth(useremail, password);
         }
-        return thisview
-        /*val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
-                R.layout.fragment_login, container, false)
 
-        binding.btnRegLogin1.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_login_to_register)
-        }
-        return binding.root*/
+        return thisview
+
 
 
     }
-    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        //mAuth = FirebaseAuth.getInstance()
-        editEmail = requireActivity().findViewById(R.id.editEmail)
-        editPassword = requireActivity().findViewById(R.id.editPassword)
-        btnLogin = requireActivity().findViewById(R.id.btLogin)
-        //btnRegLogin = requireActivity().findViewById(R.id.btnRegLogin1)
+    private fun forgetPassword(userEmail:EditText){
+         if(userEmail.text.toString().isEmpty()){
+             return
+         }
 
-        btnLogin.setOnClickListener() {
-            Toast.makeText(activity, "Testing", Toast.LENGTH_SHORT).show()
-            val useremail = editEmail.text.toString().trim()
-            val password = editPassword.text.toString().trim()
-            LoginWithAuth(useremail, password);
-        }
+        mAuth.sendPasswordResetEmail(userEmail.text.toString())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(requireActivity(), "Email sent", Toast.LENGTH_SHORT).show()
+                    }
+                }
 
-
-    }*/
+    }
 
     private fun LoginWithAuth(email: String, password: String) {
         if (email.isEmpty() && password.isEmpty()) {
-            editEmail.error = "Please enter username"
+            editEmail.error = "Please enter email address"
             editPassword.error = "Please enter password"
             return
         }
