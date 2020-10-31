608e8960de78865148f121d4d2d80d81bc47b86c
brianooi123 <ooikelvin3030@gmail.com>
2020-10-31 15:20:18 +0800

    31/10 ooi

--- app/src/main/java/com/example/neatshoe/Login.kt
+++ app/src/main/java/com/example/neatshoe/Login.kt
@@ -21,47 +21,66 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_login.*
 import java.util.regex.Pattern
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Login.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Login : Fragment() {
     lateinit var editEmail: EditText
     lateinit var editPassword: EditText
     lateinit var btnLogin: Button
+    lateinit var btnRegLogin: Button
     private lateinit var mAuth: FirebaseAuth
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        editEmail =requireActivity().findViewById(R.id.editEmail)
-        editPassword=requireActivity().findViewById(R.id.editPassword)
-        btnLogin =requireActivity().findViewById(R.id.btLogin)
+    lateinit var thisview: View
 
-        btnLogin.setOnClickListener() {
-            val useremail = editEmail.text.toString().trim()
-            val password = editPassword.text.toString().trim()
-            LoginWithAuth(useremail,password);
-        }
-    }
+
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        thisview=inflater.inflate(R.layout.fragment_login, container, false)
         mAuth = FirebaseAuth.getInstance()
-        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
-            R.layout.fragment_login,container,false)
-        binding.btnRegLogin1.setOnClickListener { view : View ->
+        btnRegLogin = thisview.findViewById(R.id.btnRegLogin1)
+        editEmail = thisview.findViewById(R.id.editEmail)
+        editPassword = thisview.findViewById(R.id.editPassword)
+        btnLogin = thisview.findViewById(R.id.btLogin)
+        // Inflate the layout for this fragment
+        btnRegLogin.setOnClickListener {view: View ->
+
             view.findNavController().navigate(R.id.action_login_to_register)
         }
-        return binding.root
+        btnLogin.setOnClickListener() {
+
+            val useremail = editEmail.text.toString().trim()
+            val password = editPassword.text.toString().trim()
+            LoginWithAuth(useremail, password);
+        }
+        return thisview
+        /*val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
+                R.layout.fragment_login, container, false)
+
+        binding.btnRegLogin1.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_login_to_register)
+        }
+        return binding.root*/
+
 
     }
+    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //mAuth = FirebaseAuth.getInstance()
+        editEmail = requireActivity().findViewById(R.id.editEmail)
+        editPassword = requireActivity().findViewById(R.id.editPassword)
+        btnLogin = requireActivity().findViewById(R.id.btLogin)
+        //btnRegLogin = requireActivity().findViewById(R.id.btnRegLogin1)
+
+        btnLogin.setOnClickListener() {
+            Toast.makeText(activity, "Testing", Toast.LENGTH_SHORT).show()
+            val useremail = editEmail.text.toString().trim()
+            val password = editPassword.text.toString().trim()
+            LoginWithAuth(useremail, password);
+        }
+
+
+    }*/
 
     private fun LoginWithAuth(email: String, password: String) {
         if (email.isEmpty() && password.isEmpty()) {
@@ -84,13 +103,13 @@ class Login : Fragment() {
                     }
                 })
     }
+
     private fun isEmailValid(email: String): Boolean {
         val expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$"
         val pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
         val matcher = pattern.matcher(email)
         return matcher.matches()
     }
-
 
 
 }
